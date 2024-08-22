e77441620b0928aa51503495ba43b1361fe3dee7
Katyurin <173839391+Katyurin@users.noreply.github.com>
2024-08-22 10:01:01 +0300

    Add files via upload
    Signed-off-by: Katyurin <173839391+Katyurin@users.noreply.github.com>

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,17 +1,15 @@
 def generate_password(n):
     password = ""
     pairs = []
+
     for i in range(1, 21):
         for j in range(i, 21):
-            if i != n and j != n:
+               if i != n and j != n:
                 pair_sum = i + j
                 if n % pair_sum == 0:
                     pairs.append(f"{i}{j}")
     password = ''.join(pairs)
     return password
-n = int(input("Введите число от 3 до 20: "))
-if 3 <= n <= 20:
+for n in range(3, 21):
     result = generate_password(n)
-    print("Нужный пароль:", result)
-else:
-    print("Число должно быть от 3 до 20.")
+    print(f"{n} - {result}")
