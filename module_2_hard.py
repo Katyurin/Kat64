975939561e7f6b639663f683bcfbb67e9faf6dbb
Katyurin <173839391+Katyurin@users.noreply.github.com>
2024-08-22 09:40:57 +0300

    Add files via upload
    Signed-off-by: Katyurin <173839391+Katyurin@users.noreply.github.com>

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,15 +1,17 @@
-def generate_password(divisor):
-    result = ''
-    for i in range(1, 11):
-        for j in range(1, 11):
-            if i != j:
+def generate_password(n):
+    password = ""
+    pairs = []
+    for i in range(1, 21):
+        for j in range(i, 21):
+            if i != n and j != n:
                 pair_sum = i + j
-                if divisor % pair_sum == 0:
-                    result += f"{i}{j}"
-    return result
+                if n % pair_sum == 0:
+                    pairs.append(f"{i}{j}")
+    password = ''.join(pairs)
+    return password
 n = int(input("Введите число от 3 до 20: "))
 if 3 <= n <= 20:
-    password = generate_password(n)
-    print(f"Сгенерированный пароль: {password}")
+    result = generate_password(n)
+    print("Нужный пароль:", result)
 else:
-    print("Ошибка: число должно быть в диапазоне от 3 до 20.")
+    print("Число должно быть от 3 до 20.")
